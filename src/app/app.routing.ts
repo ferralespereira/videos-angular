f0bf5aaff219bfdee3df68d7f41cb0adfeea675f
javier ferrales <ferralespereira@gmail.com>
2021-12-01 17:54:32 -0500

    making routes

---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+// Importar los modulos del router
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Importar componentes
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+
+// Array de tutas
+const appRoutes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'home', component:HomeComponent },
+  { path: 'login', component:LoginComponent },
+  { path: 'register', component:RegisterComponent },
+  { path: 'error', component:ErrorComponent },
+  { path: '**', component:ErrorComponent }
+];
+
+// Exportar configuracion
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
