228c26623fec767a1f08ce5b727630f2154f13e4
javier ferrales <ferralespereira@gmail.com>
2021-12-01 17:14:05 -0500

    more

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-error',
+  selector: 'error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+    public page_title: string;
+
+  constructor() { 
+    this.page_title = "Error";
+  }
 
   ngOnInit(): void {
   }
