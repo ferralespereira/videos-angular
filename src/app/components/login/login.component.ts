9777b95215bfea6f78eea49245ca5e31171abffb
javier ferrales <ferralespereira@gmail.com>
2021-12-01 17:07:51 -0500

    more

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    public page_title: string;
+
+  constructor() { 
+    this.page_title = "Login";
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    public page_title: string;
+    
+  constructor() {
+    this.page_title = "Register";
+   }
 
   ngOnInit(): void {
   }
