78817818f481901a6ad4bfa17b76fe4ab7a49338
javier ferrales <ferralespereira@gmail.com>
2021-12-08 18:35:50 -0500

    details

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [UserService]
 })
 export class HomeComponent implements OnInit {
 
   public page_title: string;
-  constructor() { 
-    this.page_title = "Home";
+  public identity:any;
+  public token:string;
+
+  constructor(
+    private _userService: UserService
+  ) { 
+    this.page_title = "Mis Videos";
+    this.identity = '';
+    this.token = '';
   }
 
   ngOnInit(): void {
+    this.loadUser();
+  }
+
+  loadUser(){
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
 }
