8cb3e628811e63a85175bc71513bf43d2722d1cd
javier ferrales <ferralespereira@gmail.com>
2021-12-15 16:23:12 -0500

    more

--- src/app/components/video-details/video-details.component.ts
+++ src/app/components/video-details/video-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { VideoService } from '../../services/video.service';
-import { Video } from '../../models/video';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-details',
@@ -17,12 +17,13 @@ export class VideoDetailsComponent implements OnInit {
   public video: any;
   public message: string;
   public status: string;
-
+  
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
     private _userService: UserService,
-    private _videoService: VideoService
+    private _videoService: VideoService,
+    private _sanitizer: DomSanitizer
   ) { 
     this.page_title = "See Video";
     this.identity = this._userService.getIdentity();
@@ -51,6 +52,18 @@ export class VideoDetailsComponent implements OnInit {
         }
       );
     });
+  }
+
+  getVideoIframe(url:string) {
+    var video, results;
+ 
+    if (url === null) {
+        return '';
+    }
+    results = url.match('[\\?&]v=([^&#]*)');
+    video   = (results === null) ? url : results[1];
+ 
+    return this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);   
   }
 
 }
