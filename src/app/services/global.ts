d6e93bdc40948b2f66ccc6a357757ce6e4735266
javier ferrales <ferralespereira@gmail.com>
2022-03-02 17:39:05 -0500

    another build

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -2,5 +2,5 @@ export var global = {
       // url: 'http://127.0.0.4/'
       // url: 'http://13.59.91.199:3999/api/'
       // url: 'https://test.javierfolder.com/api/'
-      url: 'http://videos.javierfolder.com/'
+      url: 'https://videos.javierfolder.com/'
    };
